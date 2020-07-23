d5357e98f71b7dd46be1a80e85d19e68bb7701b2
pratiksoygaonkar <pratiksoygaonkar@gmail.com>
2020-07-24 01:34:58 +0530

    minor updates done

--- Project-coronavirus-Global-tracker/src/main/java/com/pss/coronavirustracker/CoronavirusTrackerApplication.java
+++ Project-coronavirus-Global-tracker/src/main/java/com/pss/coronavirustracker/CoronavirusTrackerApplication.java
@@ -2,12 +2,19 @@ package com.pss.coronavirustracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class CoronavirusTrackerApplication {
+public class CoronavirusTrackerApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CoronavirusTrackerApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CoronavirusTrackerApplication.class, args);
 	}
